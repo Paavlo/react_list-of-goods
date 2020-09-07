179fb067168e509eb8fc340d00783f7ced93b768
Pavlo <pavlo.com.5@gmail.com>
2020-09-07 13:56:37 +0300

    Updates

---
+++ precache-manifest.ef3360737401f063624b379df37abf46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348e6c9d0f9650143fc19e914065508b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "806907e342b68ffd98d4",
+    "url": "./static/css/main.ff42c3ba.chunk.css"
+  },
+  {
+    "revision": "98d80b6ce429cf0c0270",
+    "url": "./static/js/2.4d83afd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4d83afd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806907e342b68ffd98d4",
+    "url": "./static/js/main.f41cd4d6.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
